e3771b053ec624181c2f33470851621db26eacc6
ShubhamJariwal <shubhamjariwal2@gmail.com>
2023-12-31 00:55:59 +0530

    Start github Today

--- 05_datatypes_summary.js
+++ 05_datatypes_summary.js
@@ -9,6 +9,21 @@
     6 = null
     7 = symbol
 */
+const score = 100
+const scoreValue = 100.33
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('999')
+const anotherId = Symbol('999')
+
+console.log(Id === anotherId);
+
+const bigNumber = 27994637792387403284n
+
+
 
 //  Reference (non primitive)
 
@@ -17,3 +32,16 @@
     2 = objects
     3 = functions
 */
+
+
+const heros = ["Iron man","Hulk","Thor"]; //array
+
+let myObj = {
+    name: "shubham",           //object
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
