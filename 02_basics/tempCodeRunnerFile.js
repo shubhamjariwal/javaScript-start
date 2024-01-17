a72bc39e16ec17c46916b127d16676146ae0519d
ShubhamJariwal <shubhamjariwal2@gmail.com>
2024-01-18 00:38:39 +0530

    delete tempfile

--- 02_basics/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-console.log(fb_user);
-
-// console.log(Object.keys(fb_user));
-
-// console.log(Object.values(fb_user));
-
-// console.log(Object.entries(fb_user));
-
-// console.log(fb_user.hasOwnProperty(`isLoggedIn`));
