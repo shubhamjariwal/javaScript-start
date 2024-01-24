272be764b41268f47b4a17d98518c3e369161da8
ShubhamJariwal <shubhamjariwal2@gmail.com>
2024-01-24 23:28:04 +0530

    scopes explained

--- 02_basics/03_basics/02_scopes.js
+++ 02_basics/03_basics/02_scopes.js
@@ -3,7 +3,49 @@ let a = 100
 if(true){
     let a = 10
     const b = 20
-     console.log("INNER:", a);  
+     //console.log("INNER:", a);  
 }
 
-console.log(a);
+// console.log(a);
+// console.log(b);
+// console.log(a);
+
+function one(){
+       const username = "shubham"
+        function two(){
+          const  website = "youtube"
+          console.log(username);
+        }
+        //console.log(website);
+
+        two()
+}
+
+// one()
+
+if(true){
+  const username = "shubham"
+  if(username === "shubham"){
+    const website = " youtube"
+    console.log(username + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+//////////////////////interesting/////////////////////////////
+                
+function addOne(num){
+  return num + 1
+}
+
+addOne(5)
+
+const addTwo = function(num){
+  return num + 2
+}
+
+addTwo(5)
+
+
+
